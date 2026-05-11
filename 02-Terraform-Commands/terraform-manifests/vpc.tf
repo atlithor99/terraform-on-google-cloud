cb34b5572440a728213d27eb8ec59cb9977c34e6
atlithor99 <atlithoredwald@icloud.com>
2026-05-11 14:53:51 +0200

    chore(terraform): add provider lock file and update vpc config

--- 02-Terraform-Commands/terraform-manifests/vpc.tf
+++ 02-Terraform-Commands/terraform-manifests/vpc.tf
@@ -1,20 +1,20 @@
 # Terraform Provider Configuration: google
 provider "google" {
-  project = "gcplearn9"
-  region = "us-central1"
+  project = "quality-jawfish-815045"
+  region  = "europe-north1"
 }
 
 # Resource: VPC
 resource "google_compute_network" "myvpc" {
-  name = "vpc1"
-  auto_create_subnetworks = false   
+  name                    = "vpc1"
+  auto_create_subnetworks = false
 }
 
 # Resource: Subnet
 resource "google_compute_subnetwork" "mysubnet" {
   name          = "subnet1"
-  region        = "us-central1"
+  region        = "europe-north1"
   ip_cidr_range = "10.128.0.0/20"
-  network       = google_compute_network.myvpc.id  // GET VPC ID
+  network       = google_compute_network.myvpc.id // GET VPC ID
 }
 
