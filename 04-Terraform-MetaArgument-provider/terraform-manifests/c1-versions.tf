e6af177a2cba903d71fc2af1add64033c095814b
atlithor99 <atlithoredwald@icloud.com>
2026-05-15 13:45:57 +0200

    chore(terraform): update provider config for section 04

--- 04-Terraform-MetaArgument-provider/terraform-manifests/c1-versions.tf
+++ 04-Terraform-MetaArgument-provider/terraform-manifests/c1-versions.tf
@@ -1,24 +1,25 @@
 # Terraform Settings Block
 terraform {
-  required_version = ">= 1.8"
+  required_version = "~> 1.15.2"
   required_providers {
     google = {
-      source = "hashicorp/google"
-      version = ">= 5.34.0"
+      source  = "hashicorp/google"
+      version = ">= 7.31.0"
     }
   }
 }
 
-# Terraform Provider-1: us-central1
+# Terraform Provider 1 Block
 provider "google" {
-  project = "gcplearn9"
-  region = "us-central1"
-  alias = "us-central1"    
+  project = "quality-jawfish-815045"
+  region  = "europe-north1"
+  alias   = "europe-north1"
 }
 
-# Terraform Provider-2: europe-west1
+# Terraform Provider 2 Block
 provider "google" {
-  project = "gcplearn9"
-  region = "europe-west1"
-  alias = "europe-west1"    
+  project = "quality-jawfish-815045"
+  region  = "europe-west1"
+  alias   = "europe-west1"
 }
+
--- 04-Terraform-MetaArgument-provider/terraform-manifests/c3-vpc.tf
+++ 04-Terraform-MetaArgument-provider/terraform-manifests/c3-vpc.tf
@@ -1,23 +1,23 @@
 # Resource: VPC
 resource "google_compute_network" "myvpc" {
-  project = "gcplearn9"
-  name = "vpc1"
-  auto_create_subnetworks = false   
+  project                 = "quality-jawfish-815045"
+  name                    = "vpc1"
+  auto_create_subnetworks = false
 }
 
-# Resource: Subnet1
+# Resource: Subnet
 resource "google_compute_subnetwork" "mysubnet1" {
-  provider = google.us-central1   # Define provider to use
-  name = "subnet1"
+  provider      = google.europe-north1
+  name          = "mysubnet1"
   ip_cidr_range = "10.128.0.0/20"
-  network = google_compute_network.myvpc.id 
+  network       = google_compute_network.myvpc.id
 }
 
 # Resource: Subnet2
 resource "google_compute_subnetwork" "mysubnet2" {
-  provider = google.europe-west1   # Define provider to use
-  name = "subnet2"
+  provider      = google.europe-west1 # Define provider to use
+  name          = "mysubnet2"
   ip_cidr_range = "10.132.0.0/20"
-  network = google_compute_network.myvpc.id 
+  network       = google_compute_network.myvpc.id
 }
 
