6a5f512d30858e6aa2460ee555b2aa8c2f54f0f2
atlithor99 <atlithoredwald@icloud.com>
2026-05-20 10:04:09 +0200

    chore(terraform): configure section 08 for personal gcp project

--- 08-Terraform-MetaArgument-foreach/D2-terraform-manifests/c2-variables.tf
+++ 08-Terraform-MetaArgument-foreach/D2-terraform-manifests/c2-variables.tf
@@ -2,20 +2,21 @@
 # GCP Project
 variable "gcp_project" {
   description = "Project in which GCP Resources to be created"
-  type = string
-  default = "kdaida123"
+  type        = string
+  default     = "quality-jawfish-815045"
 }
 
 # GCP Region
 variable "gcp_region1" {
   description = "Region in which GCP Resources to be created"
-  type = string
-  default = "us-east1"
+  type        = string
+  default     = "europe-north1"
 }
 
 # GCP Compute Engine Machine Type
 variable "machine_type" {
   description = "Compute Engine Machine Type"
-  type = string
-  default = "e2-small"
+  type        = string
+  default     = "e2-micro"
 }
+
--- 08-Terraform-MetaArgument-foreach/D2-terraform-manifests/c6-01-vminstance.tf
+++ 08-Terraform-MetaArgument-foreach/D2-terraform-manifests/c6-01-vminstance.tf
@@ -1,23 +1,20 @@
-# Define a map with zone as key and machine_type as value
+# Define a map variable
 variable "zone_machine_map" {
   type = map(string)
   default = {
-    "us-central1-a" = "e2-micro"
-    "us-central1-b" = "e2-small"
-    "us-central1-c" = "e2-medium"
+    "europe-north1-a" = "e2-micro"
+    "europe-north1-b" = "e2-small"
+    "europe-north1-c" = "e2-medium"
   }
 }
-# Resource Block: Create a Compute Engine instance
+# Resource Block: Create a single Compute Engine instance
 resource "google_compute_instance" "myapp1" {
-  # Meta-Argument: for_each
-  for_each = var.zone_machine_map
-  name         = "myapp1-vm-${each.key}"
+  # Meta-argument: for_each
+  for_each     = var.zone_machine_map
+  name         = "myapp1-vm-${each.key}-${each.value}"
   machine_type = each.value
   zone         = each.key
-  tags = [
-    tolist(google_compute_firewall.fw_ssh.target_tags)[0],
-    tolist(google_compute_firewall.fw_http.target_tags)[0]
-  ]
+  tags         = [tolist(google_compute_firewall.fw_ssh.target_tags)[0], tolist(google_compute_firewall.fw_http.target_tags)[0]]
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-12"
@@ -26,9 +23,10 @@ resource "google_compute_instance" "myapp1" {
   # Install Webserver
   metadata_startup_script = file("${path.module}/app1-webserver-install.sh")
   network_interface {
-    subnetwork = google_compute_subnetwork.mysubnet.id   
+    subnetwork = google_compute_subnetwork.mysubnet.id
     access_config {
       # Include this section to give the VM an external IP address
     }
   }
 }
+
