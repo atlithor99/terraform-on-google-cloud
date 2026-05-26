713f2cfdc712cd695991b1ab1511e81a3826c488
atlithor99 <atlithoredwald@icloud.com>
2026-05-26 15:28:01 +0200

    chore(terraform): configure section 13 for personal gcp project

--- 13-Regional-HTTP-LB-MIGUpdatePolicy/terraform-manifests/c6-03-app1-mig.tf
+++ 13-Regional-HTTP-LB-MIGUpdatePolicy/terraform-manifests/c6-03-app1-mig.tf
@@ -1,23 +1,22 @@
 # Resource: Managed Instance Group
 resource "google_compute_region_instance_group_manager" "myapp1" {
-  depends_on = [ google_compute_router_nat.cloud_nat ]
-  name                       = "${local.name}-myapp1-mig"
-  base_instance_name         = "${local.name}-myapp1"
-  region                     = var.gcp_region1
-  distribution_policy_zones  = data.google_compute_zones.available.names
+  name                      = "${local.name}-myapp1-mig"
+  base_instance_name        = "${local.name}-myapp1"
+  region                    = var.gcp_region1
+  distribution_policy_zones = data.google_compute_zones.available.names
   # Instance Template
   version {
     # V1 Version
     instance_template = google_compute_region_instance_template.myapp1.id
     # V2 Version
-    #instance_template = google_compute_region_instance_template.myapp1_v2.id
+    # instance_template = google_compute_region_instance_template.myapp1_v2.id
   }
   # Named Port
   named_port {
     name = "webserver"
     port = 80
   }
-  # Autosclaing
+  # Autohealing
   auto_healing_policies {
     health_check      = google_compute_region_health_check.myapp1.id
     initial_delay_sec = 300
@@ -32,5 +31,5 @@ resource "google_compute_region_instance_group_manager" "myapp1" {
     max_unavailable_fixed          = length(data.google_compute_zones.available.names)
     replacement_method             = "SUBSTITUTE"
     # min_ready_sec                  = 50 #   (BETA Parameter)
-  }  
+  }
 }
