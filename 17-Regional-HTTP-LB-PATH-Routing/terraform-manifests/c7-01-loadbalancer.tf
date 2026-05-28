faf00a755fc79d9ffdb581afbe01195952426c2f
atlithor99 <atlithoredwald@icloud.com>
2026-05-28 14:08:29 +0200

    chore(terraform): configure section 17 for personal gcp project

--- 17-Regional-HTTP-LB-PATH-Routing/terraform-manifests/c7-01-loadbalancer.tf
+++ 17-Regional-HTTP-LB-PATH-Routing/terraform-manifests/c7-01-loadbalancer.tf
@@ -25,9 +25,9 @@ resource "google_compute_region_backend_service" "myapp1" {
   health_checks         = [google_compute_region_health_check.mylb.self_link]
   port_name             = "webserver"
   backend {
-    group = google_compute_region_instance_group_manager.myapp1.instance_group
+    group           = google_compute_region_instance_group_manager.myapp1.instance_group
     capacity_scaler = 1.0
-    balancing_mode = "UTILIZATION"
+    balancing_mode  = "UTILIZATION"
   }
 }
 
@@ -39,59 +39,53 @@ resource "google_compute_region_backend_service" "myapp2" {
   health_checks         = [google_compute_region_health_check.mylb.self_link]
   port_name             = "webserver"
   backend {
-    group = google_compute_region_instance_group_manager.myapp2.instance_group
+    group           = google_compute_region_instance_group_manager.myapp2.instance_group
     capacity_scaler = 1.0
-    balancing_mode = "UTILIZATION"
+    balancing_mode  = "UTILIZATION"
   }
 }
-
 
 # Resource: Regional URL Map
 resource "google_compute_region_url_map" "mylb" {
   name            = "${local.name}-mylb-url-map"
-  default_service = google_compute_region_backend_service.myapp1.id
-
+  default_service = google_compute_region_backend_service.myapp1.self_link
   host_rule {
     hosts        = ["*"]
     path_matcher = "my-path-routing-1"
   }
-
   path_matcher {
     name            = "my-path-routing-1"
     default_service = google_compute_region_backend_service.myapp1.id
-
     path_rule {
-      paths = ["/app1/*"]
+      paths   = ["/app1/*"]
       service = google_compute_region_backend_service.myapp1.id
     }
-
     path_rule {
-      paths = ["/app2/*"]
+      paths   = ["/app2/*"]
       service = google_compute_region_backend_service.myapp2.id
     }
   }
 }
 
 
-
 # Resource: Regional HTTP Proxy
 resource "google_compute_region_target_http_proxy" "mylb" {
-  name   = "${local.name}-mylb-http-proxy"
+  name    = "${local.name}-mylb-http-proxy"
   url_map = google_compute_region_url_map.mylb.self_link
 }
 
 
 # Resource: Regional Forwarding Rule
 resource "google_compute_forwarding_rule" "mylb" {
-  name        = "${local.name}-mylb-forwarding-rule"
-  target      = google_compute_region_target_http_proxy.mylb.self_link
-  port_range  = "80"
-  ip_protocol = "TCP"
-  ip_address = google_compute_address.mylb.address
+  name                  = "${local.name}-mylb-forwarding-rule"
+  target                = google_compute_region_target_http_proxy.mylb.self_link
+  port_range            = "80"
+  ip_protocol           = "TCP"
+  ip_address            = google_compute_address.mylb.address
   load_balancing_scheme = "EXTERNAL_MANAGED" # Creates new GCP LB (not classic)
-  network = google_compute_network.myvpc.id
+  network               = google_compute_network.myvpc.id
   # During the destroy process, we need to ensure LB is deleted first, before deleting VPC proxy-only subnet
-  depends_on = [ google_compute_subnetwork.regional_proxy_subnet ]
+  depends_on = [google_compute_subnetwork.regional_proxy_subnet]
 }
 
 
